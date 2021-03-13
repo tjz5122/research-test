4f857776cfb7aa27d5e4c3997ec31b251ed78d5c
tjz5122 <54636990+tjz5122@users.noreply.github.com>
2021-03-13 15:37:11 -0500

    Update all_together.py

--- all_together.py
+++ all_together.py
@@ -1,17 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Mar  7 00:09:22 2021
-
-@author: thzha
-"""
-
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Feb  4 15:19:25 2021
-
-@author: thzha
-"""
-
 import math
 import torch
 import torch.nn as nn
@@ -134,13 +120,13 @@ class Bucket(object):
 
     # ! Need to add safeguard to allow compute only if there are enough entries
     def mean_std(self, mode='bm'):
-        mean = torch.mean(self.buffer[self.start:self.end]).item()
+        mean = torch.mean(self.buffer[self.start:self.end]).item() # mean of whole
 
         if mode == 'bm':        # batch mean variance
-            b_n = int(math.floor(math.sqrt(self.count)))
-            Yks = F.avg_pool1d(self.buffer[self.start:self.end].unsqueeze(0).unsqueeze(0), kernel_size=b_n, stride=b_n).view(-1)
+            b_n = int(math.floor(math.sqrt(self.count))) #batch size
+            Yks = F.avg_pool1d(self.buffer[self.start:self.end].unsqueeze(0).unsqueeze(0), kernel_size=b_n, stride=b_n).view(-1) # batch mean
             diffs = Yks - mean
-            std = math.sqrt(b_n /(len(Yks)-1))*torch.norm(diffs).item()
+            std = math.sqrt(b_n /(len(Yks)-1))*torch.norm(diffs).item() # len(Yks) is number of batch
             dof = b_n - 1
         elif mode == 'olbm':    # overlapping batch mean
             b_n = int(math.floor(math.sqrt(self.count)))
@@ -154,15 +140,21 @@ class Bucket(object):
 
         return mean, std, dof
 
-    def stats_test(self, sigma, tolerance, mode='bm'):
+    def stats_test(self, sigma, tolerance, mode='bm',composite_test):
         mean, std, dof = self.mean_std(mode=mode)
 
         # confidence interval
         t_sigma_dof = stats.t.ppf(1-sigma/2., dof)
-        stat = std * t_sigma_dof / math.sqrt(self.count)
-        self.statistic = stat
+        self.statistic = std * t_sigma_dof / math.sqrt(self.count)
         
-        return stat < tolerance
+        if composite_test == 2:
+            self.statistic += 0.02
+        if composite_test == 3:
+            self.statistic -= 0.02
+        if composite_test == 4:
+            self.statistic -= 0.03
+            
+        return self.statistic < tolerance
 
 
 class SSM(SSM_Optimizer):
@@ -272,25 +264,24 @@ class SSM(SSM_Optimizer):
             
             if self.state['mode'] == 'log10 loss plus smooth':
                 self.state['smoothing'] = xk1.dot(gk).item() - (0.5 * self.state['lr']) * ((1 + self.state['momemtum'])/(1 - self.state['momemtum'])) * (dk.dot(dk).item())
-                self.state['loss'] = np.log10(self.state['loss']) - 0.5
+                self.state['loss'] = np.log10(self.state['loss'])
                 self.state['stats_val'] =  self.state['loss'] + self.state['smoothing']
             
         
             if self.state['mode'] == 'In loss plus smooth':
                 self.state['smoothing'] = xk1.dot(gk).item() - (0.5 * self.state['lr']) * ((1 + self.state['momemtum'])/(1 - self.state['momemtum'])) * (dk.dot(dk).item())
-                self.state['loss'] = np.log(self.state['loss']) - 0.75
+                self.state['loss'] = np.log(self.state['loss']) 
                 self.state['stats_val'] = self.state['loss']  + self.state['smoothing']
             
             if self.state['mode'] == 'sqrt loss plus smooth':
                 self.state['smoothing'] = xk1.dot(gk).item() - (0.5 * self.state['lr']) * ((1 + self.state['momemtum'])/(1 - self.state['momemtum'])) * (dk.dot(dk).item())
-                self.state['loss'] = self.state['loss']**0.5 + 0.4
+                self.state['loss'] = self.state['loss']**0.5
                 self.state['stats_val'] =  self.state['loss'] + self.state['smoothing']
             
             if self.state['mode'] == 'sasa_plus':
                 self.state['stats_x1d'] = xk1.dot(dk).item()
                 self.state['stats_ld2'] = (0.5 * self.state['lr']) * (dk.dot(dk).item())
                 self.state['stats_val'] = self.state['stats_x1d'] + self.state['stats_ld2']
-        
         
             # add statistic to leaky bucket
             self.state['bucket'].add(self.state['stats_val'])
@@ -300,7 +291,7 @@ class SSM(SSM_Optimizer):
         self.state['stats_stationary'] = 0
         self.state['statistic'] = 0
         if self.state['bucket'].count > self.state['minN_stats'] and self.state['nSteps'] % self.state['testfreq'] == 0:
-            stationary= self.state['bucket'].stats_test(self.state['significance'], self.state['tolerance'], self.state['var_mode'])
+            stationary= self.state['bucket'].stats_test(self.state['significance'], self.state['tolerance'], self.state['var_mode'],self.state['composite_test'])
             self.state['stats_test'] = 1
             self.state['statistic'] = self.state['bucket'].statistic
             self.state['stats_stationary'] = int(stationary)
@@ -310,12 +301,8 @@ class SSM(SSM_Optimizer):
                 self.state['lr'] /= self.state['drop_factor']
                 for group in self.param_groups:
                     group['lr'] = self.state['lr']
+                self._zero_buffers('momentum_buffer')
                 self.state['bucket'].reset()
-                if self.state['composite_test'] < 4:
-                    self.state['composite_test'] += 1
-
-
-
 
 
     def _gather_flat_grad(self):
@@ -352,10 +339,238 @@ class SSM(SSM_Optimizer):
                     view = state[buf_name].data.view(-1)
                 views.append(view)
         return torch.cat(views, 0) 
+    
+    def _zero_buffers(self, buf_name):
+        for group in self.param_groups:
+            for p in group['params']:
+                state = self.state[p]
+                if buf_name in state:
+                    state[buf_name].zero_()
+        return None
+    
+    
+### Design the Mgnet Network
+use_cuda = torch.cuda.is_available()
+print('Use GPU?', use_cuda)
+
+class MgIte(nn.Module): 
+    def __init__(self, A, S):
+        super().__init__()
+        
+        self.A = A
+        self.S = S
+
+        self.bn1 =nn.BatchNorm2d(A.weight.size(0)) 
+        self.bn2 =nn.BatchNorm2d(S.weight.size(0)) 
+    
+    def forward(self, out):
+        u, f = out 
+        u = u + F.relu(self.bn2(self.S(F.relu(self.bn1((f-self.A(u))))))) 
+        out = (u, f)
+        return out
+
+
+
+class MgRestriction(nn.Module):
+    def __init__(self, A_old, A, Pi, R):
+        super().__init__()
+
+        self.A_old = A_old
+        self.A = A
+        self.Pi = Pi
+        self.R = R
+
+        self.bn1 = nn.BatchNorm2d(Pi.weight.size(0))   
+        self.bn2 = nn.BatchNorm2d(R.weight.size(0))    
+
+    def forward(self, out):
+        u_old, f_old = out 
+        u = F.relu(self.bn1(self.Pi(u_old)))                              
+        f = F.relu(self.bn2(self.R(f_old-self.A_old(u_old)))) + self.A(u)        
+        out = (u,f)
+        return out
+
+
+class MgNet(nn.Module):
+    def __init__(self, num_channel_input, num_iteration, num_channel_u, num_channel_f, num_classes):
+        super().__init__()
+        self.num_iteration = num_iteration
+        self.num_channel_u = num_channel_u
+        self.conv1 = nn.Conv2d(num_channel_input, num_channel_f, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(num_channel_f)        
+
+        
+        A = nn.Conv2d(num_channel_u, num_channel_f, kernel_size=3, stride=1, padding=1, bias=False)
+        S = nn.Conv2d(num_channel_f, num_channel_u, kernel_size=3,stride=1, padding=1, bias=False)
+        layers = []
+        for l, num_iteration_l in enumerate(num_iteration):
+            for i in range(num_iteration_l):
+                layers.append(MgIte(A, S)) 
+            setattr(self, 'layer'+str(l), nn.Sequential(*layers))
+
+            if l < len(num_iteration)-1:
+                A_old = A 
+                
+                A = nn.Conv2d(num_channel_u, num_channel_f, kernel_size=3,stride=1, padding=1, bias=False)
+                S = nn.Conv2d(num_channel_f, num_channel_u, kernel_size=3,stride=1, padding=1, bias=False)
+
+                Pi = nn.Conv2d(num_channel_u, num_channel_u, kernel_size=3,stride=2, padding=1, bias=False)
+                R = nn.Conv2d(num_channel_f, num_channel_f, kernel_size=3, stride=2, padding=1, bias=False)
+                
+                
+                layers= [MgRestriction(A_old, A, Pi, R)] 
+        
+        self.pooling = nn.AdaptiveAvgPool2d(1) 
+        self.fc = nn.Linear(num_channel_u ,num_classes) 
+
+    def forward(self, u, f):
+        f = F.relu(self.bn1(self.conv1(f)))                
+        if use_cuda:                                        
+            u = torch.zeros(f.size(0),self.num_channel_u,f.size(2),f.size(3), device=torch.device('cuda')) 
+        else:
+            u = torch.zeros(f.size(0),self.num_channel_u,f.size(2),f.size(3))        
+       
+        
+        out = (u, f) 
+
+        for l in range(len(self.num_iteration)):
+            out = getattr(self, 'layer'+str(l))(out)
+
+        
+        u, f = out       
+        u = self.pooling(u) #do avg pooling
+        u = u.view(u.shape[0], -1)  #reshape u batch_Size to vector
+        u = self.fc(u)
+        return u
+    
+    
+    
+    
+    
+    
+    
+### Implementation
+minibatch_size = 128
+num_epochs = 360
+lr = 1
+degree = 256
+num_channel_input = 3 # since cifar10
+num_channel_u = degree # usaually take channel u and f same, suggested value = 64,128,256,512.....
+num_channel_f = degree
+num_classes = 10
+num_iteration = [2,2,2,2] # for each layer do 1 iteration or you can change to [2,2,2,2] or [2,1,1,1]
+
+# Step 1: Define a model
+my_model = MgNet(num_channel_input, num_iteration, num_channel_u, num_channel_f, num_classes)
+
+if use_cuda:
+    my_model = my_model.cuda()
+
+# Step 2: Define a loss function and training algorithm
+criterion = nn.CrossEntropyLoss()
+
+
+# Step 3: load dataset
+normalize = torchvision.transforms.Normalize(mean=(0.4914, 0.4822, 0.4465), std=(0.2023, 0.1994, 0.2010))
+
+transform_train = torchvision.transforms.Compose([torchvision.transforms.RandomCrop(32, padding=4),
+                                                  torchvision.transforms.RandomHorizontalFlip(),
+                                                  torchvision.transforms.ToTensor(),
+                                                  normalize])
+
+transform_test  = torchvision.transforms.Compose([torchvision.transforms.ToTensor(),normalize])
+
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=minibatch_size, shuffle=True)
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
+testloader = torch.utils.data.DataLoader(testset, batch_size=minibatch_size, shuffle=False)
+
+
+optimizer = SSM(my_model.parameters(), lr=1, weight_decay=0.0001,momentum=0.9, testfreq=len(trainloader), mode = 'sqrt loss plus smooth', var_mode='mb', tolerance = 0.01, minN_stats=100)
 
 
 
 
+train_accuracy_list = []
+test_accuracy_list = []
+lr_list = []
+statistic_list = []
+avg_loss_list = []
+key_list = []
+
+start = timer()
+#Step 4: Train the NNs
+# One epoch is when an entire dataset is passed through the neural network only once.
+for epoch in range(num_epochs):
+    running_loss = 0
+    my_model.train()
+    for i, (images, labels) in enumerate(trainloader):
+        if use_cuda:
+          images = images.cuda()
+          labels = labels.cuda()
+
+        # Forward pass to get the loss
+        outputs = my_model(0,images)   # We need additional 0 input for u in MgNet
+        loss = criterion(outputs, labels)
+        optimizer.state['loss'] = loss.item()
+        # Backward and compute the gradient
+        optimizer.zero_grad()
+        loss.backward()  #backpropragation
+        running_loss += loss.item()
+        optimizer.step() #update the weights/parameters
+    avg_loss_list.append(running_loss)
+    
+    
+  # Training accuracy
+    my_model.eval()
+    correct = 0
+    total = 0
+    for i, (images, labels) in enumerate(trainloader):
+        with torch.no_grad():
+          if use_cuda:
+              images = images.cuda()
+              labels = labels.cuda()  
+          outputs = my_model(0,images)  # We need additional 0 input for u in MgNet, since u is initialized with 0; if do not give u a value in there, it will be reinitialized by forward function. 
+          p_max, predicted = torch.max(outputs, 1) 
+          total += labels.size(0)
+          correct += (predicted == labels).sum()
+    training_accuracy = float(correct)/total
+    train_accuracy_list.append(training_accuracy)     
+    
+    # Test accuracy
+    correct = 0
+    total = 0
+    for i, (images, labels) in enumerate(testloader):
+        with torch.no_grad():
+          if use_cuda:
+              images = images.cuda()
+              labels = labels.cuda()
+          outputs = my_model(0,images)      # We need additional 0 input for u in MgNet
+          p_max, predicted = torch.max(outputs, 1) 
+          total += labels.size(0)
+          correct += (predicted == labels).sum()
+    test_accuracy = float(correct)/total
+    test_accuracy_list.append(test_accuracy)
+    statistic_list.append(optimizer.state['statistic'])
+    key_list.append(tuple([optimizer.state['stats_val'],optimizer.state['loss'],optimizer.state['smoothing']]))
+    current_lr = optimizer.state['lr']
+    lr_list.append(current_lr)
+   
+end = timer()
+print("total computational time is", end - start)
+
+file = open("all_together.txt","x")
+file.write("train_accuracy_list_all = {}\n".format(str(train_accuracy_list)))
+file.write("test_accuracy_list_all = {}\n".format(str(test_accuracy_list)))
+file.write("lr_list_all = {}\n".format(str(lr_list)))
+file.write("statistic_list_all = {}\n".format(str(statistic_list)))
+file.write("key_list_all = {}\n".format("array(" + str(key_list) + ")"))
+file.write("avg_loss_list_all = {}\n".format(str(avg_loss_list)))
+file.write("total_time_all = {}\n".format(str(end - start)))
+file.close()
+print("complete")
 
 ### Design the Mgnet Network
 use_cuda = torch.cuda.is_available()
