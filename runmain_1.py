da7d81251dda0994bd1fd6cd3ad473fd814bebc2
tjz5122 <54636990+tjz5122@users.noreply.github.com>
2021-04-14 09:22:50 -0400

    Create runmain_1.py

---
+++ runmain_1.py
@@ -0,0 +1,64 @@
+import os
+import shlex
+import subprocess
+import threading
+import signal
+from itertools import product
+
+
+def run_command(env):
+    print('=> run commands on GPU:{}', env['CUDA_VISIBLE_DEVICES'])
+
+    while True:
+        try:
+            comm = command_list.pop(0)
+        except IndexError:
+            break
+
+        proc = subprocess.Popen(comm, env=env)
+        proc.wait()
+
+
+test_param_groups = {}
+test_param_groups["channel_group"] = [32,64,128,256]
+test_param_groups["iteration_group"] = ["1111","2111","2222"]
+test_param_groups["lr_group"] = [10.0,1.0,0.1]
+test_param_groups["wd_group"] = [0.0001,0.0005,0.001,0.005,0.01,0.05,0.1]
+test_param_groups["trail_group"] = [1,2,3]
+test_param_groups["batch_size_group"] = [64,128,256,512]
+test_param_groups["data_group"] = ["cifar10","cifar100"]
+test_param_groups["epochs_group"] = [120,240,360]
+test_param_groups["net_group"] = ["resnet18","mgnet"]
+test_param_groups["leaky_group"] = [2,4,6,8,10,12]
+test_param_groups["momentum_group"] = [0.6,0.7,0.8,0.9]
+test_param_groups["significance_group"] = [0.01,0.05,0.1]
+test_param_groups["samplefreq_group"] = [5,10,15,20]
+test_param_groups["truncate_group"] = [0.01,0.02,0.03]
+test_param_groups["ministate_group"] = [50,100,150]
+test_param_groups["keymode_group"] = ["loss_plus_smooth","loss"]
+test_param_groups["varmode_group"] = ["bm","olbm"]
+
+
+
+# flexible
+chosen_param_groups = ["channel_group","iteration_group","data_group","lr_group","batch_size_group","momentum_group","wd_group","keymode_group","varmode_group","leaky_group","ministate_group","samplefreq_group","truncate_group","significance_group","trail_group"]
+value_list = [test_param_groups[group] for group in chosen_param_groups]
+test_list = list(product(*value_list))
+command = 'python trainmain_1.py --cuda --net=mgnet --ch={} --iter={} --data={} --epochs=360 --lr={} -b={} -m={} --wd={} --km={} --vm={} --lk={} --minstat={} --sf={} --trun={} --sig={} --trail={}'
+# flexible
+
+
+command_list = []
+for file in test_list:
+    command_list += [command.format(*file)]
+command_list = [shlex.split(comm) for comm in command_list]
+
+
+# List all the GPUs you have
+
+ids_cuda = [0,1,2,3]
+for c in ids_cuda:
+    env = os.environ.copy()
+    env['CUDA_VISIBLE_DEVICES'] = str(c)
+    thread = threading.Thread(target = run_command, args = (env, ))
+    thread.start()
