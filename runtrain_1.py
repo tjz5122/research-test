25b198feaa794dca27de37aa4c7ff9161bf1ccc7
tjz5122 <54636990+tjz5122@users.noreply.github.com>
2021-06-12 20:04:02 -0400

    Update runtrain_1.py

--- runtrain_1.py
+++ runtrain_1.py
@@ -28,6 +28,7 @@ test_param_groups["iteration_group"] = ["2222"]
 test_param_groups["lr_group"] = [1.0]
 test_param_groups["wd_group"] = [0.0001,0.0005,0.001]
 test_param_groups["trail_group"] = [1,2]
+test_param_groups["drop_factor_group"] = [8,10]
 test_param_groups["batch_size_group"] = [128,256]
 test_param_groups["epochs_group"] = [240]
 test_param_groups["net_group"] = ["mgnet"]
@@ -48,7 +49,7 @@ test_param_groups["data_group"] = ["cifar10"]
 chosen_param_groups = ["channel_group","batch_size_group","momentum_group","wd_group","keymode_group","varmode_group","leaky_group","ministate_group","samplefreq_group","truncate_group","trail_group"]
 value_list = [test_param_groups[group] for group in chosen_param_groups]
 test_list = list(product(*value_list))
-command = 'python trainmain_1.py --cuda --net=mgnet --ch={} --iter=2222 --data=cifar10 --epochs=360 --lr=1 -b={} -m={} --wd={} --km={} --vm={} --lk={} --minstat={} --sf={} --trun={} --sig=0.05 --trail={}'
+command = 'python trainmain_1.py --cuda --net=mgnet --ch={} --iter=2222 --data=cifar10 --epochs=360 --lr=1 -b={} -m={} --wd={} --km={} --vm={} --lk={} --minstat={} --drop={} --sf={} --trun={} --sig=0.05 --trail={}'
 # flexible
 
 
