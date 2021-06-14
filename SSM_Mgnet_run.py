edad30d845b75db4875a4c6230bba2904261a2a6
tjz5122 <54636990+tjz5122@users.noreply.github.com>
2021-06-14 01:42:04 -0400

    Update SSM_Mgnet_run.py

--- SSM_Mgnet_run.py
+++ SSM_Mgnet_run.py
@@ -26,20 +26,22 @@ test_param_groups = {}
 test_param_groups["channel_group"] = [128,256]
 test_param_groups["iteration_group"] = ["2222"]
 test_param_groups["lr_group"] = [1.0]
-test_param_groups["wd_group"] = [0.0001,0.0005,0.001]
+test_param_groups["wd_group"] = [0.0001,0.0005,0.001] #SASA+: 0.0005
 test_param_groups["trail_group"] = [1,2]
-test_param_groups["drop_factor_group"] = [2,5,10] #SASA+
+test_param_groups["drop_factor_group"] = [5,10]           #SASA+: 2,5,10
 test_param_groups["batch_size_group"] = [128,256]
 test_param_groups["epochs_group"] = [240]
-test_param_groups["leaky_group"] = [2,4,8,16]     #SASA+
+test_param_groups["leaky_group"] = [4,8,12]                 #SASA+: 2,4,8,16
 test_param_groups["momentum_group"] = [0.6,0.8]
-test_param_groups["significance_group"] = [0.001,0.01,0.05,0.1] #SASA+
+test_param_groups["significance_group"] = [0.05]          #SASA+ : 0.001,0.01,0.05,0.1
 test_param_groups["samplefreq_group"] = [5,10,15]
 test_param_groups["truncate_group"] = [0.02,0.03]
 test_param_groups["ministate_group"] = [50,100]
 test_param_groups["keymode_group"] = ["loss_plus_smooth"]
 test_param_groups["varmode_group"] = ["bm"]
 test_param_groups["data_group"] = ["cifar10"]
+
+
 
 
 # flexible
@@ -57,6 +59,7 @@ command_list = [shlex.split(comm) for comm in command_list]
 
 
 # List all the GPUs you have
+
 ids_cuda = [0,1,2,3]
 for c in ids_cuda:
     env = os.environ.copy()
